3d14fb7a0136c34053cc69970fb029e3ce76c4dd
“Nikhil T” <nikhil.t.career@gmail.com>
2021-12-31 22:15:04 +0530

    Application

---
+++ src/main/java/my/smvc/hib/NikhilEmployeeMgmtRestApiSecurityAsgApplication.java
@@ -0,0 +1,13 @@
+package my.smvc.hib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NikhilEmployeeMgmtRestApiSecurityAsgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NikhilEmployeeMgmtRestApiSecurityAsgApplication.class, args);
+	}
+
+}
